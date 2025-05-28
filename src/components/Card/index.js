55b3952f888e17fb9aef28e5857e90b7949ada36
SophiaAlbuquerque <sophiaalbuquerqueeleuterio@gmail.com>
2025-05-28 20:01:14 -0300

    feat: page adote um pet

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,30 @@
-import { useFavoritoContext } from 'contextos/Favoritos';
-import styles from './Card.module.css';
-import iconeFavoritar from './favoritar.png';
-import iconeDesfavoritar from './desfavoritar.png';
-import { Link } from 'react-router-dom';
+import { useFavoritoContext } from "contextos/Favoritos";
+import styles from "./Card.module.css";
+import iconeFavoritar from "./favoritar.png";
+import iconeDesfavoritar from "./desfavoritar.png";
 
-function Card({ id, titulo, capa }) {
-    console.log(id, titulo, capa)
-    const { favorito, adicionarFavorito } = useFavoritoContext();
-    const ehFavorito = favorito.some((fav) => fav.id === id);
-    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
-    return (
-        <div className={styles.container}>
-            <Link className={styles.link} to={`/${id}`}>
-                <img src={capa} alt={titulo} className={styles.capa} />
-                <h2>{titulo}</h2>
-            </Link>
-            <img src={icone}
-                alt="Favoritar filme"
-                className={styles.favoritar}
-                onClick={() => {
-                    adicionarFavorito({ id, titulo, capa })
-                }} />
+function Card({ id, nome, descricao, imagem }) {
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  const ehFavorito = favorito.some((fav) => fav.id === id);
+  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
+  return (
+    <div className={styles.card}>
+      <img src={imagem} alt={nome} className={styles.imagem} />
+      <div className={styles.texto}>
+        <div className={styles.header}>
+          <h2 className={styles.nome}>{nome}</h2>
+          <img
+            src={icone}
+            alt="Favoritar"
+            className={styles.favoritar}
+            onClick={() => adicionarFavorito({ id, nome, descricao, imagem })}
+          />
         </div>
-
-    )
+        <p>{descricao}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
