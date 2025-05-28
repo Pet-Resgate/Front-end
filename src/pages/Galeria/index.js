d87cb8306efb7c76a2338e977d2e68fe4308eefd
SophiaAlbuquerque <sophiaalbuquerqueeleuterio@gmail.com>
2025-05-27 23:44:51 -0300

    feat: start header, nav, and footer (80%) (Adote um Pet pages in progress)

---
+++ src/pages/Galeria/index.js
@@ -0,0 +1,15 @@
+import Banner from "Components/Banner";
+import Titulo from "Components/Titulo";
+
+function Galeria() {
+  return (
+    <>
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Adote um Pet</h1>
+      </Titulo>
+    </>
+  );
+}
+
+export default Galeria;
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,23 +1,48 @@
-import Banner from "components/Banner";
-import Card from "components/Card";
-import Titulo from "components/Titulo";
-import videos from 'json/db.json';
-import styles from './Inicio.module.css';
+import Banner from "Components/Banner";
+import Card from "Components/Card";
+import Titulo from "Components/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
+import Carousel from "Components/Carousel";
+import logo from "./logo.png";
 
 function Inicio() {
-    return (
-        <>
-            <Banner imagem="home" />
-            <Titulo>
-                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
-            </Titulo>
-            <section className={styles.container}>
-                {videos.map((video) => {
-                    return <Card {...video} key={video.id} />
-                })}
-            </section>
-        </>
-    )
+  return (
+    <>
+      <Carousel>
+        <div
+          style={{
+            backgroundColor: "var(--amarelo_principal)",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={logo} alt="Logo do cinetag" height="100%"></img>
+        </div>
+        <div style={{ height: "100%" }}>
+          <Banner imagem="home" />
+        </div>
+        <div
+          style={{
+            backgroundColor: "var(--amarelo_principal)",
+            height: "100%",
+          }}
+        >
+          Slide 3
+        </div>
+      </Carousel>
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Inicio;
